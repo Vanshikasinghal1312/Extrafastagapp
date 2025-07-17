df879427c53cca7bf98e0de96b9c72df11e32e47
Vanshikasinghal1312 <145016869+Vanshikasinghal1312@users.noreply.github.com>
2025-07-17 16:40:43 +0530

    Add files via upload

---
+++ metro.config.js
@@ -0,0 +1,36 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+
+// module.exports = {
+//   transformer: {
+//     getTransformOptions: async () => ({
+//       transform: {
+//         experimentalImportSupport: false,
+//         inlineRequires: true,
+//       },
+//     }),
+//   },
+// };
+const { getDefaultConfig } = require('metro-config');
+
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts }
+  } = await getDefaultConfig();
+
+  return {
+    transformer: {
+      assetPlugins: ['expo-asset/tools/hashAssetFiles'],
+    },
+    resolver: {
+      assetExts: [...assetExts, 'png'],
+      sourceExts,
+    },
+  };
+})();
+
